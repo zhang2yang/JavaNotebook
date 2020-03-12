c694d9c2c4c05a94f3304e94ab6d0f81906ac59f
Geligamesh <535758155@qq.com>
2020-03-12 21:36:47 +0800

    AQS

---
+++ src/com/gxb/threadcoreknowledge/aqs/OneShotLatch.java
@@ -0,0 +1,35 @@
+package com.gxb.threadcoreknowledge.aqs;
+
+import sun.misc.GC;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * 用AQS实现一个简单的线程协作器
+ */
+public class OneShotLatch {
+
+    private final Sync sync = new Sync();
+
+    public void signal() {
+        sync.releaseShared(0);
+    }
+
+    public void await() {
+        sync.acquireShared(0);
+    }
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+        @Override
+        protected int tryAcquireShared(int arg) {
+            return getState() == 1 ? 1 : -1;
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int arg) {
+            setState(1);
+            return true;
+        }
+    }
+}
